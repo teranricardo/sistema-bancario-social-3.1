41f73a955a7c334417762f4a25e8fc646a636738
Adolf-2005 <adolfovasquezr@hotmail.com>
2024-07-28 21:01:03 -0400

    Manejar rutas de usuarios por autentificación y roles

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var usersController = require("../controllers/users.c");
+const { verifyToken, verifyRole } = require('../middlewares/auth');
 
 /* GET listar usuarios */
 router.get('/', (req, res) => usersController.show(req, res));
-
-/* POST crear usuario */
-router.post('/', (req, res) => usersController.create(req, res));
-
-/* GET mostrar formulario para crear usuario */
-router.get('/new', (req, res) => res.render('users/new'));
 
 /* GET mostrar usuario por id */
 router.get('/:id', (req, res) => usersController.showByID(req, res, edit = false));
 
 /* GET mostrar usuario por id */
-router.get('/:id/edit', (req, res) => usersController.showByID(req, res, edit = true));
+router.get('/:id/edit', verifyToken, verifyRole('admin'), (req, res) => usersController.showByID(req, res, edit = true));
 
 /* PUT editar usuario */
-router.put('/:id', (req, res) => usersController.edit(req, res));
+router.put('/:id', verifyToken, verifyRole('admin'), (req, res) => usersController.edit(req, res));
+
+/* GET confirmación de eliminación */
+router.get('/:id/delete', verifyToken, verifyRole('admin'), (req, res) => {
+  const userId = req.params.id;
+  res.render('users/delete', { userId });
+});
 
 /* DELETE eliminar usuario */
-router.delete('/:id', (req, res) => usersController.delete(req, res));
+router.delete('/:id', verifyToken, verifyRole('admin'), (req, res) => usersController.delete(req, res));
 
 /* GET cuentas del usuario */
-router.get('/:id/accounts', (req, res) => usersController.getAccounts(req, res));
+router.get('/:id/accounts', verifyToken, verifyRole('admin'), (req, res) => usersController.getAccounts(req, res));
 
 /* GET resumen de cuentas del usuario */
-router.get('/:id/accounts/summary', (req, res) => usersController.summaryAccounts(req, res));
+router.get('/:id/accounts/summary', verifyToken, verifyRole('admin'), (req, res) => usersController.summaryAccounts(req, res));
 
 module.exports = router;
